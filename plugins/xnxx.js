bc178a520dc38a0af8bf5add100eefe44079f439
uvaktrading-star <uvaktrading@gmail.com>
2026-02-22 18:20:11 +0530

    Update xnxx.js

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const API_KEY = "darknero";
 const SEARCH_API = "https://apis.sandarux.sbs/api/download/xnxx-search";
 const DL_API = "https://apis.sandarux.sbs/api/download/xnxx-dl";
-const LOGO_URL = "https://github.com/Akashkavindu/ZANTA_MD/blob/main/images/zanta-md.png?raw=true";
+const LOGO_URL = "https://raw.githubusercontent.com/Akashkavindu/MINI-BOT-SOURCE/main/zanta-md.png";
 
 cmd({
     pattern: "xnxx",
